d17396d417678d72d5bbe2ac0fdc32c0ec7cf44a
tomas b <motioncgraphic@gmail.com>
2015-04-30 18:24:33 +0200

    005

---
+++ ConsoleApplication1/MainApp.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Game.h"
+#include <SFML/Graphics.hpp>
+
+int main(int argc, _TCHAR* argv[])
+{
+	Game::Start();
+	//hey
+
+	return 0;
+}
